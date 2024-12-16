8d8fc9c389a460726829490ccf4684acdbbe548b
勝　大地 <daichikatsu@katsudaichinoMacBook-puro.local>
2024-12-16 23:05:10 +0900

    Convert config files to CommonJS format

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+const { defineConfig } = require('vite');
+const react = require('@vitejs/plugin-react');
 
-// https://vitejs.dev/config/
-export default defineConfig({
+module.exports = defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  build: {
+    outDir: 'dist/client',
+    emptyOutDir: true
+  }
 });
